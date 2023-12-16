111c351fef548fd37e0a0c2ea9d2b393aa59a600
Shibraj <shibrajdeb456@gmail.com>
2023-12-16 17:15:27 +0530

    basic setup and test done

--- test/ETHDaddy.js
+++ test/ETHDaddy.js
@@ -1,9 +1,16 @@
-const { expect } = require("chai")
+/* eslint-disable no-undef */
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
-}
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
 
 describe("ETHDaddy", () => {
-
-})
+  it("has a name", async () => {
+    const ETHDaddy = await ethers.getContractFactory("ETHDaddy");
+    let ethdaddy = await ETHDaddy.deploy();
+    const result = await ethdaddy.name();
+    expect(result).to.equal("Eth daddy");
+  });
+});
