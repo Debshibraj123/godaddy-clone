c6fac03ce3a053f8664b8b3bacf94aacbecebb9f
Shibraj <shibrajdeb456@gmail.com>
2023-12-17 16:57:15 +0530

    web3 part done

--- src/App.js
+++ src/App.js
@@ -14,13 +14,23 @@ import config from './config.json';
 
 function App() {
 
+  const [account, setAccount] = useState(null)
+  const loadBlockChainData = async () => {
+    const accounts =  await window.ethereum.request({ 'method'   :  'eth_requestAccounts'})
+    const account = ethers.utils.getAddress(accounts[0])
+    setAccount(account)
+  }
+  useEffect(() => {
+    loadBlockChainData();
+  }, [])
+  
+
   return (
     <div>
 
       <div className='cards__section'>
-
         <h2 className='cards__title'>Welcome to ETH Daddy</h2>
-
+        <p>{account}</p>
       </div>
 
     </div>
